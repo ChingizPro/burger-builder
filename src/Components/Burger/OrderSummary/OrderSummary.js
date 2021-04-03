7436a00d53fecc33e71f0ba0828d8b2241c0e393
ChingizPro <chingizprog@gmail.com>
2021-04-03 14:39:42 +0500

    Add UI folder for modal and backdrop

---
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Wrapper from '../../../hoc/Wrapper/Wrapper';
+
+const OrderSummary = (props) => {
+    return (
+        <Wrapper>
+            <h1>Here is your order</h1>
+        </Wrapper>
+    );
+}
+
+export default OrderSummary;
