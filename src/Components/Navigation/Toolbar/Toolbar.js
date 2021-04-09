dc0569bb3a7e786e1b97b76f910f5b99537068c1
ChingizPro <chingizprog@gmail.com>
2021-04-09 17:21:29 +0500

    Create reusable nav items and use them to build a menu

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
-import Logo from '../../Logo/Logo';
 import classes from './Toolbar.module.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const Toolbar = props => (
     <div className={classes.Toolbar}>
         <div>Menu icon</div>
         <Logo />
         <nav>
-            nav links
+            <NavigationItems />
         </nav>
     </div>
 );
