e06ba943c569a63d48427f72d35b5a57b04f60ae
ChingizPro <chingizprog@gmail.com>
2021-04-09 08:37:44 +0500

    Remove props from Logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -3,7 +3,7 @@ import React from 'react';
 import logoSource from '../../assets/images/burger-logo.png';
 import classes from './Logo.module.css';
 
-const Logo = props => (
+const Logo = () => (
     <div className={classes.Logo}>
         <img src={logoSource} />
     </div>
