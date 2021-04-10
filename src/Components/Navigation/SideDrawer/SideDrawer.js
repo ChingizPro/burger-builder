ad93c13b6e889011c8cc9a4cb3647ca18846dc3d
ChingizPro <chingizprog@gmail.com>
2021-04-10 14:56:13 +0500

    Add Backdrop to the SideDrawer

--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -3,15 +3,20 @@ import React from 'react';
 import Logo from '../../../Components/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Wrapper from '../../../hoc/Wrapper/Wrapper';
 
 const SideDrawer = props => {
     return (
-        <div className={classes.SideDrawer}>
-            <Logo className={classes.abc} height='10%' />
-            <nav className={classes.nav}>
-                <NavigationItems />
-            </nav>
-        </div>
+        <Wrapper>
+            <Backdrop visible />
+            <div className={classes.SideDrawer}>
+                <Logo className={classes.abc} height='10%' />
+                <nav className={classes.nav}>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </Wrapper>
     );
 }
 
