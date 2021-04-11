e64c270dcd3c355b1d4ca8daf193d4f71dfdd776
ChingizPro <chingizprog@gmail.com>
2021-04-11 12:08:04 +0500

    Create a separate toggler component for sidedrawer menu

---
+++ src/Components/Navigation/SideDrawer/MenuToggler/MenuToggler.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MenuToggler = props => (
+    <div onClick={props.clicked}><em>toggler</em></div>
+)
+
+export default MenuToggler;
--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import MenuToggler from '../SideDrawer/MenuToggler/MenuToggler';
 
 const Toolbar = props => (
     <div className={classes.Toolbar}>
-        <div onClick={props.menuClick}>Menu icon</div>
+        <MenuToggler clicked={props.menuClick} />
         <Logo height='77%' />
         <nav className={classes.DesktopOnly}>
             <NavigationItems />
