68416455898f49dd1c0fc5b2adb3516adcb987b3
ChingizPro <chingizprog@gmail.com>
2021-04-11 12:23:01 +0500

    Style hamburger icon for menu toggler

--- src/Components/Navigation/SideDrawer/MenuToggler/MenuToggler.js
+++ src/Components/Navigation/SideDrawer/MenuToggler/MenuToggler.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import classes from './MenuToggler.module.css';
+
 const MenuToggler = props => (
-    <div onClick={props.clicked}><em>toggler</em></div>
+    <div className={classes.Toggler} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 )
 
 export default MenuToggler;
