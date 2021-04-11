60295f15894ede5f14e7024178c238a714f71be1
ChingizPro <chingizprog@gmail.com>
2021-04-11 11:30:46 +0500

    Transfer Layout functional component into stateful Class component

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,18 +1,26 @@
+import React, { Component } from 'react';
+
 import Wrapper from '../../hoc/Wrapper/Wrapper'
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.module.css';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => {
-    return (
-        <Wrapper>
-            <SideDrawer />
-            <Toolbar />
-            <main className={classes.content}>
-                {props.children}
-            </main>
-        </Wrapper>
-    )
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <SideDrawer />
+                <Toolbar />
+                <main className={classes.content}>
+                    {this.props.children}
+                </main>
+            </Wrapper>
+        )
+    }
 }
 
 export default Layout;
