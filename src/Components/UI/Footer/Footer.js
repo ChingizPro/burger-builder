9230d161f015e36a5b4c249713ee4eaefaeab1ff
ChingizPro <chingizprog@gmail.com>
2021-04-14 08:52:16 +0500

    Optimize footer links

--- src/Components/UI/Footer/Footer.js
+++ src/Components/UI/Footer/Footer.js
@@ -4,8 +4,8 @@ import classes from './Footer.module.css';
 
 const Footer = () => (
     <div className={classes.Footer}>
-        <p>Made by: ChingizPro | <a href='chingizpro.uz' target='_blank'>Developer's website</a></p>
-        <p>Favicon designed by 千图网 from <a href='https://pngtree.com/' target='_blank'>Pngtree.com</a></p>
+        <p>Made by: ChingizPro | <a href='https://chingizpro.uz' target='_blank' rel='noreferrer'>Developer's website</a></p>
+        <p>Favicon designed by 千图网 from <a href='https://pngtree.com/' target='_blank' rel='noreferrer'>Pngtree.com</a></p>
     </div>
 );
 
