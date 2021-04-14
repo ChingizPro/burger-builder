57d61e2a3b117d73bf73d12e8a3e42ab5378477c
ChingizPro <chingizprog@gmail.com>
2021-04-14 09:10:50 +0500

    Change Modal to check update cycle

--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Wrapper from '../../../hoc/Wrapper/Wrapper';
 import Backdrop from '../Backdrop/Backdrop';
 import classes from './Modal.module.css';
 
-const Modal = (props) => (
-    <Wrapper>
-        <Backdrop visible={props.showModal} click={props.hideModal} />
-        <div className={classes.Modal} style={{ transform: props.showModal ? 'translateY(0vh)' : 'translateY(-100vh)' }}>
-            {props.children}
-        </div>
-    </Wrapper>
-);
+class Modal extends Component {
+    componentDidUpdate() {
+        console.log('[Modal] update!');
+    }
+
+    render() {
+        return (
+            <Wrapper>
+                <Backdrop visible={this.props.showModal} click={this.props.hideModal} />
+                <div className={classes.Modal} style={{ transform: this.props.showModal ? 'translateY(0vh)' : 'translateY(-100vh)' }}>
+                    {this.props.children}
+                </div>
+            </Wrapper>
+        );
+    }
+};
 
 export default Modal;
